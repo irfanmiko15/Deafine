10a41ad1a9d55276778537804c3a8ec4e4115d82
Irfan Dary <irfandarys@gmail.com>
2023-08-14 09:06:21 +0700

    Initial Commit

---
+++ ASLClasifier/ASLClasifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLClasifierApp.swift
+//  ASLClasifier
+//
+//  Created by Irfan Dary Sujatmiko on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ASLClasifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
